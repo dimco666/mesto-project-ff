7961accde019fcf659bc5e3dfc090cc20c25f3b4
dimco666 <dimco666@yandex.ru>
2024-04-17 21:11:34 +0300

    остановился на открытии попапа с картинкой

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,15 +15,17 @@ const addButton = content.querySelector('.profile__add-button');
 const cardTemplate = document.querySelector('#card-template').content;
 const editButton = content.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
-const closeButton = document.querySelectorAll('.popup__close');
+const closeButton = document.querySelector('.popup__close');
 const popupNewCard = document.querySelector('.popup_type_new-card');
 const popupImage = document.querySelector('.popup_type_image');
 const popups = document.querySelectorAll('.popup');
+const modalImage = popupImage.querySelector('.popup__image');
+const modalImageCaption = popupImage.querySelector('.popup__caption');
 
 import { initialCards } from './cards.js';
 
 initialCards.forEach(function(item) {
-  const cardItem = createCard(item, deleteCard);
+  const cardItem = createCard(item, deleteCard, handleClickCard);
   placesList.append(cardItem);
 });
 
@@ -31,7 +33,13 @@ export function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-export function createCard(item, deleteCard) {
+function handleClickCard(evt) {
+  const imageUrl = evt.link;
+  const imageAlt = evt.name;
+  openPopup(popupImage);
+}
+
+export function createCard(item, deleteCard, handleClickCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
@@ -45,8 +53,12 @@ export function createCard(item, deleteCard) {
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', handleDeleteButtonClick);
+
+    modalImage.addEventListener('click', () => {handleClickCard(evt)});
+
   return cardElement;
 }
+
 //функции открытия и закрытия попапа
 function openPopup(modalPopup) {
   modalPopup.classList.add('popup_is-opened');
@@ -64,10 +76,20 @@ addButton.addEventListener('click', () => {
   openPopup(popupNewCard)
 });
 //слушатели закрытия
+/*
 closeButton[0].addEventListener('click', () => {
   closePopup(popupEdit)
 });
 
 closeButton[1].addEventListener('click', () => {
   closePopup(popupNewCard)
+});
+*/
+
+popups.forEach((modalPopup) => {
+  modalPopup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close')) {
+      closePopup(modalPopup);
+    }
+  });
 });
